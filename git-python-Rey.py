2717577ef74479101b7ce453114fc45cc1aafb33
ReyIanReniel <ianreneilrey25@gmail.com>
2023-03-10 19:37:04 +0800

    My Educational Background added

--- git-python-Rey.py
+++ git-python-Rey.py
@@ -1,3 +1,9 @@
 print ("Ian Reniel Rey")
 print ("Brgy.Bagong Silang Regalia street Santa Cruz Marinduque")
 print ("June 25, 2003")
+print ("Marinduque State College")
+print ("Bachelor of Science in Information Technology (BSIT)")
+print ("2nd Year")
+print ("Section: 2B")
+
+
